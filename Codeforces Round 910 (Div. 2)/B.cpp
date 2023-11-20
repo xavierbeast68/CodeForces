65313463181966d0bd61113b0c5c02f02405b1b0
xavierbeast68 <sdby101@gmail.com>
2023-11-21 01:46:37 +0530

    update

---
+++ Codeforces Round 910 (Div. 2)/B.cpp
@@ -0,0 +1,50 @@
+/*
+    ▄  ██       ▄   ▄█ ▄███▄   █▄▄▄▄ ███   ▄███▄   ██      ▄▄▄▄▄      ▄▄▄▄▀ 
+▀▄   █ █ █       █  ██ █▀   ▀  █  ▄▀ █  █  █▀   ▀  █ █    █     ▀▄ ▀▀▀ █    
+  █ ▀  █▄▄█ █     █ ██ ██▄▄    █▀▀▌  █ ▀ ▄ ██▄▄    █▄▄█ ▄  ▀▀▀▀▄       █    
+ ▄ █   █  █  █    █ ▐█ █▄   ▄▀ █  █  █  ▄▀ █▄   ▄▀ █  █  ▀▄▄▄▄▀       █     
+█   ▀▄    █   █  █   ▐ ▀███▀     █   ███   ▀███▀      █              ▀      
+ ▀       █     █▐               ▀                    █                      
+        ▀      ▐                                    ▀                       
+*/
+/**
+ * xavierbeast68
+ * URL : 
+ * AVOIDING COMPLEXITY, REDUCES BUGS.
+ */
+
+/*
+    *Thought Process*
+    !---------------!
+    
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+// <---------- Global Variables ---------->
+
+// <-------------------------- Helper Functions -------------------------->
+
+// <-------------------------- Solve Function -------------------------->
+
+void solve(){
+
+}
+
+// <-------------------------- Main -------------------------->
+signed main(){
+    
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    
+    int testcases = 1;
+    // cin >> testcases;
+    while(testcases--){
+        solve();
+    }
+    
+    return 0;
+}
